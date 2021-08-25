8e4dbb802040dd6a77328808d471388ed867382e
Thamyris <thamyrissatana3@gmail.com>
2021-08-25 11:12:41 -0300

    param added

--- services/api-request.js
+++ services/api-request.js
@@ -6,6 +6,9 @@ export const getMovieDb = async (page = 1) => {
       api_key: process.env.NEXT_PUBLIC_API_KEY,
       language: "pt-BR",
       page: page,
+      sort_by: "vote_average.desc",
+      primary_release_year: 2021,
+      "vote_count.gte": 1000,
     },
   });
   return req.data;
