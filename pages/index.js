467126d2708b6184bc9d9a389603036f2fa1cad9
Thamyris <thamyrissatana3@gmail.com>
2021-08-26 00:15:25 -0300

    ranking page update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-import styles from "../styles/Home.module.css";
-import { useEffect, useState, useCallback } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
 import { getMovieDb } from "../services/api-request";
+import { Button } from "../components/button/Button";
+
+import styles from "../styles/Home.module.scss";
+import { HiStar, HiChevronRight, HiChevronLeft } from "react-icons/hi";
 
 export default function Home() {
   const [page, setPage] = useState(1);
@@ -32,22 +37,40 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      <ol>
+      <ol start={(page - 1) * 20 + 1} className={styles.ol}>
         {movieList.results?.map((movie) => {
           return (
-            <div key={movie.id}>
-              <li>{movie.title}</li>
+            <div className={styles.movieContainer} key={movie.id}>
+              <li className={styles.li}>
+                <Link
+                  className={styles.link}
+                  cursor="pointer"
+                  href={`/${movie.id}?title=${movie.original_title}`}
+                  passHref
+                >
+                  <a className={styles.movieTitle}>{movie.title}</a>
+                </Link>
+              </li>
+              <span className={styles.vote}>
+                <HiStar className={styles.HiStar} />
+                {movie.vote_average}
+              </span>
             </div>
           );
         })}
       </ol>
-      <button onClick={getPreviousPage} disabled={page === 1}>
-        {"<"}
-      </button>
-      <button onClick={getNextPage} disabled={movieList.total_pages === page}>
-        {" "}
-        {">"}{" "}
-      </button>
+      <footer className={styles.footer}>
+        <Button
+          onClick={getPreviousPage}
+          disabled={page === 1}
+          text={<HiChevronLeft />}
+        />
+        <Button
+          onClick={getNextPage}
+          disabled={movieList.total_pages === page}
+          text={<HiChevronRight />}
+        />
+      </footer>
     </div>
   );
 }
